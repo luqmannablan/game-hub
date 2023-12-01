05b2528618346d4a0b069ce44ccb71f545715518
Luqman <luqmannablan@gmail.com>
2023-12-01 17:32:14 -0600

    Refactor Duplicated Styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,18 @@
+import React, { ReactNode } from 'react'
+import { Box } from '@chakra-ui/react'
+
+interface Props {
+    children: ReactNode
+}
+
+const GameCardContainer = ({ children }: Props) => {
+
+    // this is where we design the place for our cards
+    return (
+        <Box width='300px' borderRadius='10px' overflow='hidden'>
+            {children}
+        </Box>
+    )
+}
+
+export default GameCardContainer
