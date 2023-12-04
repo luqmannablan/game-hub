86537058df93d7c7f4a730113c49bb20e62965d4
Luqman <luqmannablan@gmail.com>
2023-12-04 11:05:26 -0600

    Highlighted Selected Genre

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       </GridItem>
       <Show above='lg'>
         <GridItem area='aside' padding={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area='main' >
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,10 +6,11 @@ import getCroppedImageUrl from '../services/image-url'
 
 interface Props {
     onSelectGenre: (genre: Genre) => void
+    selectedGenre: Genre | null
 }
 
 // added onselectGenre to notify the parent that a genre has been slected
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
 
     const { data, isLoading, error } = useGenres()
 
@@ -21,7 +22,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
                 <ListItem key={genre.id} paddingY='5px'>
                     <HStack>
                         <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
-                        <Button onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+                        <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
                     </HStack>
                 </ListItem>)}
         </List>
